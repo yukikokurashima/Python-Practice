4ab6742e7a08acdd6628e4f4773beadba4b64586
Yukiko Kurashima <yukikokukiko@gmail.com>
2024-11-05 02:38:09 -0500

    adding files

--- Python6.1.py
+++ Python6.1.py
@@ -1,1 +1,11 @@
-print("hello world")
+#Create a program that asks the user to enter their name and their age.
+#Print out a message addressed to them that tells them the year 
+#that they will turn 100 years old. 
+#Note: for this exercise, the expectation is that 
+#you explicitly write out the year (and therefore be out of date the next year).
+
+
+name=input('What is your name?')
+age=int(input('What is your age?'))
+year=2024+(100-age)
+print(name + ",you will be 100 years old in the year " + str(year))
---
+++ Python6.2.py
@@ -0,0 +1,11 @@
+#Ask the user for a number. 
+#Depending on whether the number is even or odd, 
+#print out an appropriate message to the user. 
+#Hint: how does an even / odd number react differently when divided by 2?
+
+num=int(input("please enter the number"))
+if num %2==0:
+    print("it is prime numbers")
+else:
+    print("odd")
+
